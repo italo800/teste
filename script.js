75694b4b9e2948ca2c69f9091db0ea21a49e47f6
italo800 <ffcww47a3dx@gmail.com>
2025-01-10 12:41:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Função para exibir uma mensagem ao clicar em um botão
+function showAlert() {
+    alert("Botão clicado!");
+}
+
+// Adiciona um evento de clique a todos os botões na página
+document.query
